f53f8c4f5e6dafa0b295c382b274f5efaf797042
Vladimir <shues@bk.ru>
2018-06-19 00:01:50 +0300

    dostavka

---
+++ security.php
@@ -0,0 +1,30 @@
+<?php
+	class Security {
+		function user_create() {
+		}
+		
+		function user_change(){
+		}
+		
+		function user_delete() {
+		}
+		
+		function user_login() {
+		}
+		
+		function user_loguot() {
+		}
+		
+		private function set_login() {
+		}
+		
+		private function set_password() {
+		}
+		
+		private function check_login() {
+		}
+		
+		private function check_password() {
+		}
+	}
+?>
