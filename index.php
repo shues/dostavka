2a904816c9b205d7dcaf8f93c482fd67a59da43e
Vladimir <shues@bk.ru>
2018-06-21 02:57:07 +0300

    react

--- index.php
+++ index.php
@@ -3,13 +3,18 @@
     При входе на сайт, пользователь стартует новую сессию.
     При этом, в параметре user массива session хранится имя пользвателя.
     */
-	
+   session_start();
+	/*echo '<pre>';
+		print_r($_SESSION);
+	echo '</pre>';*/
 	if (isset($_SESSION['user_name'])){
         $user_name = $_SESSION['user_name'];
-        echo "Hello $user_name";
+        require_once('user_cont.php');
     }
     else{
-        session_start();
+        /*echo '<pre>';
+        	print_r($_SESSION);
+        echo '</pre>';*/
         require_once('autorization.php');
     }
 	
---
+++ js/main.js
@@ -0,0 +1,4 @@
+ReactDOM.render(
+	React.DOM.h1(null, "Hello World!"),
+	document.getElementById("app")
+);
